ce47f3eafe9212e13df854427cd499dd0732fb7a
nnarek <49597137+nnarek@users.noreply.github.com>
2019-07-19 22:01:01 +0400

    Add files via upload

---
+++ 27)tanks_number_estimation.py
@@ -0,0 +1,31 @@
+from math import factorial as fac
+from random import randint as rd
+
+def f(k):
+	p=0
+	mx=max(k)
+	m=len(k)
+	i=mx
+	while (p<0.99):
+		p+=fac(i-m)*(m-1)*fac(mx-1)/(fac(i)*fac(mx-m))
+		i+=1
+	return (mx,i-1,p);
+
+N=1000
+m=50
+h=[x+1 for x in range(N)]
+k=[]
+for x in range(m):
+	r=rd(0,len(h)-1)
+	k.append(h[r])
+	del h[r]
+print(k)
+print(f(k))
+
+#կաջող ենք ավեկւ պաջզեցնռլ,քանի որ մեզ k-ից միայն մքասն ու քանակն է օետք 
+#h-ն էլ պետք չի
+
+
+
+
+
